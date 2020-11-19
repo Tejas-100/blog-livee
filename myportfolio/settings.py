88854c0530bb360e70c125a9b721ba99d41b6fc1
tejas <tejasgotavade@gmail.com>
2020-11-19 22:59:38 +0530

    bkbbn

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -74,22 +74,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'myportfolio.wsgi.application'
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['console'],
-            'level': os.getenv('DJANGO_LOG_LEVEL', 'ERROR'),
-        },
-    },
-}
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
